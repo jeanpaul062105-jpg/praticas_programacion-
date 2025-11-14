e267f80c2b3ec34761179b44df129dc255ed3fae
Jeanpaul <jeanpaul062105@gmail.com>
2025-11-14 12:29:58 +0100

    Add HolaMundo.java file

--- git add HolaMundo.java
+++ git add HolaMundo.java
@@ -1,2 +1,7 @@
 # praticas_programacion-
    git clone https://github.com/tu_usuario/practicas_programacion.git    cd practicas_programacion
+public class HolaMundo {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
